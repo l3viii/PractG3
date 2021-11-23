47d9bd56028c81109c10711a08993296c07e8abd
g3teg <g3teg@DESKTOP-SIIESEU>
2021-11-23 10:13:30 +0300

    Добавьте файлы проекта.

---
+++ Lib_11/Calculation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibArray;
+
+namespace Lib_11
+{
+    public static class Calculation
+    {
+        public static string FindMaxNumber(this MyArray myarray)
+        {
+            int[] array = new int[myarray.RowLength];
+
+            for (int i = 0; i < myarray.ColumnLength; i++)
+            {
+                int minValue = 100;
+
+                for (int j = 0; j < myarray.RowLength; j++)
+                {
+
+                    if (myarray[j, i] < minValue)
+                    {
+                        minValue = myarray[j, i];
+                    }
+
+                    if (j == myarray.RowLength - 1)
+                    {
+                        array = array.Concat(new int[] { minValue }).ToArray();
+                    }
+                }
+            }
+            return array.Max().ToString();
+        }
+    }
+}
---
+++ Ptact_3G/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibArray;
+using Lib_11;
+
+namespace Ptact_3G
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private MyArray _myArray = new MyArray(3, 3);
+
+        private void Calculation_Click(object sender, RoutedEventArgs e)
+        {
+            result.Text = _myArray.FindMaxNumber();
+        }
+
+        private void FillArray_Click(object sender, RoutedEventArgs e)
+        {
+            result.Clear();
+            _myArray.Fill();
+            dataGridMain.ItemsSource = _myArray.ToDataTable().DefaultView;
+        }
+
+        private void ClearArray_Click(object sender, RoutedEventArgs e)
+        {
+            _myArray.Clear();
+            result.Clear();
+            dataGridMain.ItemsSource = _myArray.ToDataTable().DefaultView;
+        }
+
+        private void SaveArray_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовый документ (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            saveFileDialog.DefaultExt = ".txt";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.Title = "Экспорт массива";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                _myArray.Export(saveFileDialog.FileName);
+            }
+            result.Clear();
+            dataGridMain.ItemsSource = null;
+        }
+
+        private void OpenArray_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовый документ (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            openFileDialog.DefaultExt = ".txt";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Title = "Импорт массива";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _myArray.Import(openFileDialog.FileName);
+            }
+            result.Clear();
+            dataGridMain.ItemsSource = _myArray.ToDataTable().DefaultView;
+        }
+
+        private void Information_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнил Дроздов Г. ИСП-34.", "О программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
